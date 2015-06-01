66bde59af94800b78313facdd8d8c7e18ad5283d
Adam K Dean <adam@amberfish.co.uk>
2015-06-01 18:55:00 +0100

    Test commit

--- start.sh
+++ start.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-PRIVATE_IPV4=$(ifconfig eth1 | grep "inet addr" | awk -F: '{print $2}' | awk '{print $1}')
-
 docker build -t publisherd .
 
 docker run --rm -ti \
     --link ambassador:ambassador \
-    -p $PRIVATE_IPV4:80:80 \
+    -p 80:80 \
     -e BACKEND_8500=consul-8500.service.consul \
     publisherd
-
-echo "*** Listening on $PRIVATE_IPV4:80 ***"
