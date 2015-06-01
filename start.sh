d0e9eac1614fed9c8afbca3f9db08c92ae2da7fe
Adam K Dean <adam@amberfish.co.uk>
2015-06-01 19:11:07 +0100

    Test commit

--- start.sh
+++ start.sh
@@ -6,4 +6,5 @@ docker run --rm -ti \
     --link ambassador:ambassador \
     -p 80:80 \
     -e BACKEND_8500=consul-8500.service.consul \
+    -e BACKEND_8000=hello-world.service.consul \
     publisherd
