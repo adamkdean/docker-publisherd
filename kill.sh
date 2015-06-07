61cee153207299e32136f518532a056f29284616
Adam K Dean <adam@amberfish.co.uk>
2015-06-07 16:08:50 +0100

    Kill script no longer removes publisherd-data

--- kill.sh
+++ kill.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#
+# Remove all publisherd containers (not the data container though)
 
-docker ps -a | grep publisherd | awk '{print $1}' | xargs docker kill
-docker ps -a | grep publisherd | awk '{print $1}' | xargs docker rm
+docker ps -a | grep publisherd | grep -v data | awk '{print $1}' | xargs docker kill
+docker ps -a | grep publisherd | grep -v data | awk '{print $1}' | xargs docker rm
